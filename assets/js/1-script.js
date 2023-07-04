f87e3e93cb2046b6a5bdb7fbd562829b76340df7
Bruno Tenório <tenoriobn@gmail.com>
2023-07-04 16:48:13 -0300

    Implementando valores de forma dinâmica (pageviews e price)

--- assets/js/1-script.js
+++ assets/js/1-script.js
@@ -1,4 +1,6 @@
 let range = document.querySelector(".input__range");
+let priceLabel = document.querySelector(".price-month");
+let pageviewsLabel = document.querySelector(".pageviews")
 
 function progressBar() {
     range.addEventListener("input", () => {
@@ -11,5 +13,41 @@ function progressBar() {
     })
 }
 
-progressBar()
+function priceControl() {
+    fetch('./assets/js/prices.json')
+    .then(response => response.json())
+    .then(prices => {
+        const priceValues = prices.map(item => item.price);
+        const pageViews = prices.map(item => item.pageviews);
 
+        range.setAttribute('max', priceValues.length);
+
+        range.addEventListener('input', (event) => {
+            const priceIndex = parseInt(event.target.value) - 1;
+            const price = priceValues[priceIndex];
+            priceLabel.textContent = `$${price}`;
+
+            const view = pageViews[priceIndex];
+            pageviewsLabel.textContent = `${view} Pageviews`
+
+            console.log(price)
+            console.log(pageViews)
+        });
+
+        updatePriceLabel(priceValues, pageViews);
+    })
+    .catch(error => {
+        console.error('Erro ao carregar o arquivo JSON:', error);
+    });
+}
+
+function updatePriceLabel(priceValues, pageViews) {
+    const initialPrice = priceValues[0];
+    priceLabel.textContent = `$${initialPrice}`
+
+    const initialPage = pageViews[0];
+    pageviewsLabel.textContent = `${initialPage} Pageviews`
+}
+
+progressBar();
+priceControl();
